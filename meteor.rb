499707abbaa9c3e5693f6ab08f0290727e8668b4
caktux <caktux@gmail.com>
2015-12-07 12:40:45 -0500

    update meteor to 1.2.1 symlink ~/.meteor to Cellar

--- meteor.rb
+++ meteor.rb
@@ -3,18 +3,20 @@ require "formula"
 class Meteor < Formula
   homepage "https://www.meteor.com"
   head 'https://github.com/meteor/meteor.git'
-  url "https://d3sqy0vbqsdhku.cloudfront.net/packages-bootstrap/1.0.2/meteor-bootstrap-os.osx.x86_64.tar.gz",
-    :using => :nounzip
-  sha1 "084021e8b75a73a84fff97a525740fbcbb2a75ee"
-  version "1.0.2"
-
-  # sha1 "e3c74028ffaf089408bea52a4eb06ff42cac4462"
-  # version "1.0.3.1"
-  # 1.0.3.1 fails the initial install, 1.0.2 seems to work and updates itself
+  url "https://d3sqy0vbqsdhku.cloudfront.net/packages-bootstrap/1.2.1/meteor-bootstrap-os.osx.x86_64.tar.gz", :using => :nounzip
+  sha256 'fa816178cb03f79d924d3915680fba3ebc934466d7ec92384024434a6f8c3b3d'
+  version "1.2.1"
 
   depends_on "node" if build.head?
 
   def install
+    if Dir.exists?("/Users/#{ENV['USER']}/.meteor")
+      odie <<-EOS.undent
+        ~/.meteor already exists. Please remove it
+        or move it to a backup location first.
+      EOS
+    end
+
     system "tar xvfz meteor-bootstrap-os.osx.x86_64.tar.gz" unless build.head?
     system "mv .meteor meteor" unless build.head?
 
@@ -22,17 +24,22 @@ class Meteor < Formula
 
     prefix.install Dir["meteor/*"]
     bin.install_symlink prefix/"meteor"
+
+    ln_s prefix, "/Users/#{ENV['USER']}/.meteor"
   end
 
   def caveats
     s = <<-EOS.undent
-      Installed meteor 1.0.2 for the initial run,
+      Installed meteor 1.2.1 for the initial run,
       which will update itself to the latest version.
 
-      The brew formula will stay at 1.0.2.
+      The brew formula will stay at 1.2.1
 
       If you upgrade this formula, you might want to
       run `meteor update` after upgrading.
+
+      A symlink from /Users/#{ENV['USER']}/.meteor
+      to #{prefix} was installed.
     EOS
     s unless build.head?
   end
